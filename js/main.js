08baa6362f94b4a1ccb34cdc9e57caa2d94e34ce
Adam Barnes <adambarnes5000@gmail.com>
2017-09-10 15:59:31 +0100

    Added oscilloscope

--- js/main.js
+++ js/main.js
@@ -16,26 +16,34 @@ window.onload = function () {
         var audioStream = audioContent.createMediaStreamSource(stream);
         var analyser = audioContent.createAnalyser();
         audioStream.connect(analyser);
-        analyser.fftSize = BARS_COUNT*4;
+        analyser.fftSize = BARS_COUNT * 4;
+        analyser.smoothingTimeConstant = 0.8;
+        analyser.minDecibels = -100;
         var frequencyArray = new Uint8Array(analyser.frequencyBinCount);
+        var timeArray = new Uint8Array(analyser.frequencyBinCount);
 
         var doDraw = function () {
-
             requestAnimationFrame(doDraw);
             analyser.getByteFrequencyData(frequencyArray);
+            analyser.getByteTimeDomainData(timeArray);
             var adjustedLength;
-            var barWidth = Math.floor(context.canvas.width/BARS_COUNT);
-            var barDisplayWidth = Math.floor(barWidth*0.9);
+            var adjustedLength2;
+            var barWidth = Math.floor(context.canvas.width / BARS_COUNT);
+            var barDisplayWidth = Math.floor(barWidth * 0.9);
             var barHeight = context.canvas.height
-            var barHeightUnit = Math.floor(barHeight/256)
+            var barHeightUnit = Math.floor(barHeight / 256)
             for (var i = 1; i <= BARS_COUNT; i++) {
                 adjustedLength = frequencyArray[i - 1] * barHeightUnit;
+                adjustedLength2 = timeArray[i - 1] * barHeightUnit;
 
                 context.fillStyle = "#000000";
-                context.fillRect((i+12) * barWidth, 0, barWidth, barHeight);
+                context.fillRect((i+16) * barWidth, 0, barWidth, barHeight);
 
                 context.fillStyle = gradient;
-                context.fillRect((i+12) * barWidth, barHeight - adjustedLength, barDisplayWidth, adjustedLength);
+                context.fillRect((i+16) * barWidth, barHeight - adjustedLength, barDisplayWidth, adjustedLength);
+
+                context.fillStyle = "#ffffff";
+                context.fillRect((i+16) * barWidth, (barHeight/2) - adjustedLength2, barWidth, 1);
             }
         }
         doDraw();
