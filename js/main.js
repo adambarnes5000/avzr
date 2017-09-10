a6a19ccdb678fb61bc0fc974a69c92d52bc9e298
Adam Barnes <adambarnes5000@gmail.com>
2017-09-10 12:49:03 +0100

    Bar size is now dynamic

--- js/main.js
+++ js/main.js
@@ -1,48 +1,51 @@
+const BARS_COUNT = 256;
+
 window.onload = function () {
     "use strict";
     var canvas = document.getElementById("avCanvas");
-    var ctx = canvas.getContext("2d");
-    
-    // Create gradient
-    var grd = ctx.createLinearGradient(0,0,0,800);
-    grd.addColorStop(0,"red");
-    grd.addColorStop(1,"green");
-    
-    // Fill with gradient
-    ctx.fillStyle = grd;
-    
+    var context = canvas.getContext("2d");
 
-    var soundAllowed = function (stream) {
+    var gradient = context.createLinearGradient(0, 0, 0, 800);
+    gradient.addColorStop(0, "red");
+    gradient.addColorStop(1, "green");
+    context.fillStyle = gradient;
+
+    var startAnalyzing = function (stream) {
         window.persistAudioStream = stream;
         var audioContent = new AudioContext();
         var audioStream = audioContent.createMediaStreamSource(stream);
         var analyser = audioContent.createAnalyser();
         audioStream.connect(analyser);
-        analyser.fftSize = 32;
+        analyser.fftSize = BARS_COUNT*4;
         var frequencyArray = new Uint8Array(analyser.frequencyBinCount);
 
         var doDraw = function () {
+
             requestAnimationFrame(doDraw);
             analyser.getByteFrequencyData(frequencyArray);
             var adjustedLength;
-            for (var i = 1; i <= frequencyArray.length; i++) {
-                adjustedLength = frequencyArray[i-1] * 3;
-                ctx.fillStyle="#000000";
-                ctx.fillRect(i*100, 0, 100, 800);
-                
-                
-                ctx.fillStyle=grd;
-                ctx.fillRect(i*100, 800-adjustedLength, 80, adjustedLength);
+            var barWidth = Math.floor(context.canvas.width/BARS_COUNT);
+            var barDisplayWidth = Math.floor(barWidth*0.9);
+            var barHeight = context.canvas.height
+            var barHeightUnit = Math.floor(barHeight/256)
+            for (var i = 1; i <= BARS_COUNT; i++) {
+                adjustedLength = frequencyArray[i - 1] * barHeightUnit;
+
+                context.fillStyle = "#000000";
+                context.fillRect((i+12) * barWidth, 0, barWidth, barHeight);
+
+                context.fillStyle = gradient;
+                context.fillRect((i+12) * barWidth, barHeight - adjustedLength, barDisplayWidth, adjustedLength);
             }
         }
         doDraw();
     }
 
-    var soundNotAllowed = function (error) {
+    var showError = function (error) {
         alert("You must allow your microphone.");
         console.log(error);
     }
 
-    navigator.getUserMedia({ audio: true }, soundAllowed, soundNotAllowed);
+    navigator.getUserMedia({ audio: true }, startAnalyzing, showError);
 
 };
